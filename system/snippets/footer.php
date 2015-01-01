368b494d3a5bb86b8add8c9df8f2562a3d85f678
markseu <mark2011@mayberg.se>
2015-01-02 00:20:05 +0100

    Core update (Happy new year)

--- system/snippets/footer.php
+++ system/snippets/footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
-<a href="<?php echo $yellow->page->base."/" ?>">&copy; 2014 <?php echo $yellow->page->getHtml("sitename") ?></a>.
+<a href="<?php echo $yellow->page->base."/" ?>">&copy; 2015 <?php echo $yellow->page->getHtml("sitename") ?></a>.
 <a href="http://datenstrom.se/yellow">Made with Yellow</a>.
 </div>
 </div>
