c8bdc7862dd8f7d744b4bc73761f61c0d8bda606
markseu <mark2011@mayberg.se>
2015-07-06 21:31:51 +0200

    Core update (localhost support)

--- system/core/core-webinterface.php
+++ system/core/core-webinterface.php
@@ -5,7 +5,7 @@
 // Web interface core plugin
 class YellowWebinterface
 {
-	const Version = "0.5.18";
+	const Version = "0.5.19";
 	var $yellow;				//access to API
 	var $active;				//web interface is active? (boolean)
 	var $userLoginFailed;		//web interface login failed? (boolean)
@@ -624,26 +624,25 @@ class YellowWebinterfaceUsers
 	{
 		if($this->isExisting($email))
 		{
+			$serverScheme = $this->yellow->config->get("webinterfaceServerScheme");
+			$serverName = $this->yellow->config->get("webinterfaceServerName");
 			$location = $this->yellow->config->get("serverBase").$this->yellow->config->get("webinterfaceLocation");
-			$domain = $this->yellow->config->get("webinterfaceServerName");
 			$expire = time()+60*60*24*30*365;
 			$session = $this->yellow->toolbox->createHash($this->users[$email]["hash"], "sha256");
 			if(empty($session)) $session = "error-hash-algorithm-sha256";
-			if($domain == "localhost") $domain = $expire = false;
-			setcookie($cookieName, "$email,$session", $expire, $location, $domain,
-				$this->yellow->config->get("webinterfaceServerScheme")=="https");
+			if($serverName == "localhost") $serverName = false;
+			setcookie($cookieName, "$email,$session", $expire, $location, $serverName, $serverScheme=="https");
 		}
 	}
 	
 	// Destroy browser cookie
 	function destroyCookie($cookieName)
 	{
+		$serverScheme = $this->yellow->config->get("webinterfaceServerScheme");
+		$serverName = $this->yellow->config->get("webinterfaceServerName");
 		$location = $this->yellow->config->get("serverBase").$this->yellow->config->get("webinterfaceLocation");
-		$domain = $this->yellow->config->get("webinterfaceServerName");
-		$expire = time()-3600;
-		if($domain == "localhost") $domain = $expire = false;
-		setcookie($cookieName, "", $expire, $location, $domain,
-			$this->yellow->config->get("webinterfaceServerScheme")=="https");
+		if($serverName == "localhost") $serverName = false;
+		setcookie($cookieName, "", time()-3600, $location, $serverName, $serverScheme=="https");
 	}
 	
 	// Return information from browser cookie
