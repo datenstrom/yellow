7e9e1f97b62596ebc36f1cc7b3bdd373192b6385
markseu <mark2011@mayberg.se>
2016-09-04 14:22:53 +0200

    Yellow requires PHP 5.3 or higher

--- yellow.php
+++ yellow.php
@@ -2,6 +2,8 @@
 // Yellow is for people who make websites. http://datenstrom.se/yellow
 // This file may be used and distributed under the terms of the public license.
 
+version_compare(PHP_VERSION, "5.3", '>=') || die("Yellow requires PHP 5.3 or higher!");
+
 require_once("system/plugins/core.php");
 if(PHP_SAPI!="cli")
 {
