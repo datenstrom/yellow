13c7a11e6760570a1d268fa20e280945aa3536ea
markseu <mark2011@mayberg.se>
2016-11-01 23:03:02 +0100

    System update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Yellow 0.6.5
+Yellow 0.6.6
 ============
 [![Yellow](https://raw.githubusercontent.com/datenstrom/yellow-developers/master/media/images/yellow-photo.jpg)](https://datenstrom.se/yellow)
 
--- system/plugins/core.php
+++ system/plugins/core.php
@@ -5,7 +5,7 @@
 // Yellow core
 class YellowCore
 {
-	const VERSION = "0.6.5";
+	const VERSION = "0.6.6";
 	var $page;				//current page
 	var $pages;				//pages from file system
 	var $files;				//files from file system
