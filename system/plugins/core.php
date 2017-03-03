d925580b924889d0d40f81f8ba8df9579f57ce78
markseu <mark2011@mayberg.se>
2017-03-03 12:15:19 +0100

    System update

--- system/plugins/core.php
+++ system/plugins/core.php
@@ -5,7 +5,7 @@
 
 class YellowCore
 {
-	const VERSION = "0.6.8";
+	const VERSION = "0.6.9";
 	var $page;				//current page
 	var $pages;				//pages from file system
 	var $files;				//files from file system
