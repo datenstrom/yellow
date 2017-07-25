1389210f9441b0b7cbcafc1df88e2586768c8187
markseu <mark2011@mayberg.se>
2017-07-25 23:46:22 +0200

    System update (multi language remix)

--- system/themes/assets/flatsite.php
+++ system/themes/assets/flatsite.php
@@ -5,7 +5,7 @@
 
 class YellowThemeFlatsite
 {
-	const VERSION = "0.6.13";	
+	const VERSION = "0.7.1";	
 }
 
 $yellow->themes->register("flatsite", "YellowThemeFlatsite", YellowThemeFlatsite::VERSION);
