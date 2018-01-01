4e5ab96ed3e55dc834387fd8d3472a2d3c1e9e51
markseu <mark2011@mayberg.se>
2018-01-01 11:31:58 +0100

    Happy new year

--- system/themes/snippets/footer.php
+++ system/themes/snippets/footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
 <div class="siteinfo">
-<a href="<?php echo $yellow->page->base."/" ?>">&copy; 2017 <?php echo $yellow->page->getHtml("sitename") ?></a>.
+<a href="<?php echo $yellow->page->base."/" ?>">&copy; 2018 <?php echo $yellow->page->getHtml("sitename") ?></a>.
 <a href="<?php echo $yellow->page->get("pageEdit") ?>">Edit</a>.
 <a href="<?php echo $yellow->text->get("yellowUrl") ?>">Made with Yellow</a>.
 </div>
