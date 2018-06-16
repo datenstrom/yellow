b068054b38aaed1184b1b3ecf2282d9a5df0f1ff
markseu <mark2011@mayberg.se>
2018-06-16 21:44:16 +0200

    Updated plugins and language files

--- system/plugins/core.php
+++ system/plugins/core.php
@@ -5,7 +5,7 @@
 
 class YellowCore
 {
-	const VERSION = "0.7.3";
+	const VERSION = "0.7.4";
 	var $page;				//current page
 	var $pages;				//pages from file system
 	var $files;				//files from file system
--- system/plugins/language.php
+++ system/plugins/language.php
@@ -5,7 +5,7 @@
 
 class YellowLanguage
 {
-	const VERSION = "0.7.10";
+	const VERSION = "0.7.11";
 }
 
 $yellow->plugins->register("language", "YellowLanguage", YellowLanguage::VERSION);
