0e8d30c397e961f212ea98764d1206075534ddba
markseu <mark2011@mayberg.se>
2018-08-11 00:24:39 +0200

    Datenstrom Yellow requires PHP 5.4 or higher

--- yellow.php
+++ yellow.php
@@ -8,15 +8,14 @@ extension_loaded("mbstring") || die("Datenstrom Yellow requires PHP mbstring ext
 extension_loaded("curl") || die("Datenstrom Yellow requires PHP cURL extension!");
 extension_loaded("zip") || die("Datenstrom Yellow requires PHP zip extension!");
 require_once("system/plugins/core.php");
-if(PHP_SAPI!="cli")
-{
-	$yellow = new YellowCore();
-	$yellow->load();
-	$yellow->request();
+
+if (PHP_SAPI!="cli") {
+    $yellow = new YellowCore();
+    $yellow->load();
+    $yellow->request();
 } else {
-	$yellow = new YellowCore();
-	$yellow->load();
-	$statusCode = $yellow->command($argv[1], $argv[2], $argv[3], $argv[4], $argv[5], $argv[6], $argv[7]);
-	exit($statusCode<400 ? 0 : 1);
+    $yellow = new YellowCore();
+    $yellow->load();
+    $statusCode = $yellow->command($argv[1], $argv[2], $argv[3], $argv[4], $argv[5], $argv[6], $argv[7]);
+    exit($statusCode<400 ? 0 : 1);
 }
-?>
