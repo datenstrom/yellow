6f2990bbd44e75d4477cc95b921564bdb7050b0c
markseu <mark2011@mayberg.se>
2019-01-02 11:53:52 +0100

    Happy new year

--- system/themes/snippets/footer.php
+++ system/themes/snippets/footer.php
@@ -1,6 +1,6 @@
 <div class="footer" role="contentinfo">
 <div class="siteinfo">
-<a href="<?php echo $yellow->page->base."/" ?>">&copy; 2018 <?php echo $yellow->page->getHtml("sitename") ?></a>.
+<a href="<?php echo $yellow->page->base."/" ?>">&copy; 2019 <?php echo $yellow->page->getHtml("sitename") ?></a>.
 <a href="<?php echo $yellow->text->get("yellowUrl") ?>">Made with Datenstrom Yellow</a>.
 </div>
 </div>
