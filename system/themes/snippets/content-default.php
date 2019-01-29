5fd0d04d50b268d5ced8bca8c4bcddf9a64cd90e
markseu <mark2011@mayberg.se>
2019-01-29 15:09:36 +0100

    updated templates and snippets

--- system/themes/snippets/content-default.php
+++
@@ -1,7 +0,0 @@
-<div class="content">
-<?php $yellow->snippet("sidebar") ?>
-<div class="main" role="main">
-<h1><?php echo $yellow->page->getHtml("titleContent") ?></h1>
-<?php echo $yellow->page->getContent() ?>
-</div>
-</div>
