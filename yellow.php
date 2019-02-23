363da1629401aa9ad6107ed76ad002af38ee0ade
markseu <mark2011@mayberg.se>
2019-02-23 15:06:52 +0100

    Datenstrom Yellow requires PHP 5.5 or higher

--- yellow.php
+++ yellow.php
@@ -1,13 +1,13 @@
 <?php
-// Datenstrom Yellow is for people who make websites. https://datenstrom.se/yellow/
-// Copyright (c) 2013-2018 Datenstrom, https://datenstrom.se
+// Datenstrom Yellow is for people who make websites, https://datenstrom.se/yellow/
+// Copyright (c) 2013-2019 Datenstrom, https://datenstrom.se
 // This file may be used and distributed under the terms of the public license.
 
-version_compare(PHP_VERSION, "5.4", ">=") || die("Datenstrom Yellow requires PHP 5.4 or higher!");
+version_compare(PHP_VERSION, "5.5", ">=") || die("Datenstrom Yellow requires PHP 5.5 or higher!");
 extension_loaded("mbstring") || die("Datenstrom Yellow requires PHP mbstring extension!");
 extension_loaded("curl") || die("Datenstrom Yellow requires PHP cURL extension!");
 extension_loaded("zip") || die("Datenstrom Yellow requires PHP zip extension!");
-require_once("system/plugins/core.php");
+require_once("system/extensions/core.php");
 
 if (PHP_SAPI!="cli") {
     $yellow = new YellowCore();
