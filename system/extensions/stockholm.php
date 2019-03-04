345b168b6f99fcd4b35c7668969692fda1055775
markseu <mark2011@mayberg.se>
2019-03-04 12:55:24 +0100

    Renamed theme Flatsite to Stockholm

--- system/extensions/stockholm.php
+++ system/extensions/stockholm.php
@@ -1,9 +1,9 @@
 <?php
-// Flatsite extension, https://github.com/datenstrom/yellow-extensions/tree/master/themes/flatsite
+// Stockholm extension, https://github.com/datenstrom/yellow-extensions/tree/master/themes/stockholm
 // Copyright (c) 2013-2019 Datenstrom, https://datenstrom.se
 // This file may be used and distributed under the terms of the public license.
 
-class YellowFlatsite {
+class YellowStockholm {
     const VERSION = "0.8.2";
     const TYPE = "theme";
 }
