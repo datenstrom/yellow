8685e7084f3b18974738cf9cf2a2e3eed14714be
markseu <mark2011@mayberg.se>
2020-02-09 11:14:53 +0100

    Updated themes

--- system/extensions/stockholm.php
+++ system/extensions/stockholm.php
@@ -1,10 +1,10 @@
 <?php
 // Stockholm extension, https://github.com/datenstrom/yellow-extensions/tree/master/themes/stockholm
-// Copyright (c) 2013-2019 Datenstrom, https://datenstrom.se
+// Copyright (c) 2013-2020 Datenstrom, https://datenstrom.se
 // This file may be used and distributed under the terms of the public license.
 
 class YellowStockholm {
-    const VERSION = "0.8.6";
+    const VERSION = "0.8.7";
     const TYPE = "theme";
     public $yellow;         //access to API
     
