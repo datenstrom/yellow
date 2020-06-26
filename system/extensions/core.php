753eebbb0ce624770271ae8d532313d29b3b2f1c
markseu <mark2011@mayberg.se>
2020-06-26 02:42:56 +0200

    Updated extensions for new API, tip

--- system/extensions/core.php
+++ system/extensions/core.php
@@ -86,7 +86,7 @@ class YellowCore {
             ini_set("display_errors", 1);
             error_reporting(E_ALL);
         }
-        error_reporting(E_ALL ^ E_NOTICE); //TODO: remove later, for backwards compatibility
+        error_reporting(E_ALL ^ E_NOTICE); //TODO: remove this line and test your extensions, for backwards compatibility
     }
     
     // Handle initialisation
