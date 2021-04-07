c184353d75969661e44e3b2711082aa7a7d1e899
markseu <mark2011@mayberg.se>
2021-04-08 00:47:19 +0200

    Updated themes, previousnext

--- system/extensions/stockholm.php
+++ system/extensions/stockholm.php
@@ -2,7 +2,7 @@
 // Stockholm extension, https://github.com/datenstrom/yellow-extensions/tree/master/source/stockholm
 
 class YellowStockholm {
-    const VERSION = "0.8.10";
+    const VERSION = "0.8.11";
     public $yellow;         // access to API
     
     // Handle initialisation
