ec27937ade419ab02aa7e40a22e0289f337014cc
markseu <mark2011@mayberg.se>
2023-08-30 21:27:13 +0200

    Updated core, text description without footnotes

--- system/extensions/core.php
+++ system/extensions/core.php
@@ -2,7 +2,7 @@
 // Core extension, https://github.com/annaesvensson/yellow-core
 
 class YellowCore {
-    const VERSION = "0.8.115";
+    const VERSION = "0.8.116";
     const RELEASE = "0.8.22";
     public $content;        // content files
     public $media;          // media files
@@ -2398,7 +2398,9 @@ class YellowToolbox {
                     $lengthMax = 0;
                 }
                 if ($lengthMax<=0 || !$elementFound) break;
-                if ($hiddenLevel>0 || preg_match("/aria-hidden=\"true\"/i", $elementAttributes)) {
+                if ($hiddenLevel>0 ||
+                    preg_match("/aria-hidden=\"true\"/i", $elementAttributes) ||
+                    preg_match("/role=\"doc-noteref\"/i", $elementAttributes)) {
                     if (!is_string_empty($elementName) && is_string_empty($elementEnd) && !in_array(strtolower($elementName), $elementsVoid)) {
                         if (is_string_empty($elementStart)) {
                             ++$hiddenLevel;
