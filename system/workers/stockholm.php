23e99a671ea7d763c8f6e178bfb61335bc773c59
markseu <mark2011@mayberg.se>
2024-04-27 11:03:09 +0200

    Updated CSS for table alignment

--- system/workers/stockholm.php
+++ system/workers/stockholm.php
@@ -2,7 +2,7 @@
 // Stockholm extension, https://github.com/annaesvensson/yellow-stockholm
 
 class YellowStockholm {
-    const VERSION = "0.9.1";
+    const VERSION = "0.9.2";
     public $yellow;         // access to API
     
     // Handle initialisation
